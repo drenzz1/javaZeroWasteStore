22780314a52fc2085ac37865360ba7e9424be906
drenzz1 <you@example.com>
2023-08-09 23:15:20 +0200

    Dreni Mac Commit

---
+++ src/main/java/com/zerowaste/ecommerce/entity/Product.java
@@ -0,0 +1,27 @@
+package com.zerowaste.ecommerce.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "product")
+@Data
+public class Product {
+    @Id
+    private long id ;
+    private String sku ;
+    private String name ;
+    private String description ;
+    private BigDecimal unitPrice ;
+    private String imageUrl ;
+    private boolean active ;
+    private int unitsInStock ;
+    private Date dateCreated ;
+    private Date lastUpdated ;
+}
