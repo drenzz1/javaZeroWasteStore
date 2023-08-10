74c8dcfbcf22887347dfce578e90686beb76f503
drenzz1 <you@example.com>
2023-08-10 17:08:38 +0200

    Dreni commit

---
+++ angular-zerowaste/src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+import {Product} from "../../common/product";
+import {ProductService} from "../../service/product.service";
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit{
+  products:Product[]=[];
+
+  constructor(private productService:ProductService) {
+  }
+  ngOnInit() {
+    this.listProducts();
+
+  }
+  listProducts(){
+    this.productService.getProductList()
+  }
+}
