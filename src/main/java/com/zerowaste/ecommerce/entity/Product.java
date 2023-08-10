72a48af62343771cd0df3f039b50788b5c678df9
drenzz1 <you@example.com>
2023-08-10 08:23:32 +0200

    Dreni Mac Commit

--- src/main/java/com/zerowaste/ecommerce/entity/Product.java
+++ src/main/java/com/zerowaste/ecommerce/entity/Product.java
@@ -1,11 +1,11 @@
 package com.zerowaste.ecommerce.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
-
+import lombok.Generated;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import java.math.BigDecimal;
 import java.util.Date;
 
@@ -14,14 +14,40 @@ import java.util.Date;
 @Data
 public class Product {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private long id ;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id",nullable = false)
+    private ProductCategory category;
+
+    @Column(name = "sku")
     private String sku ;
+
+    @Column(name = "name")
     private String name ;
+
+    @Column(name = "description")
     private String description ;
+
+    @Column(name = "unit_price")
     private BigDecimal unitPrice ;
+
+    @Column(name = "image_url")
     private String imageUrl ;
+
+    @Column(name = "active")
     private boolean active ;
+
+    @Column(name = "units_in_stock")
     private int unitsInStock ;
+
+    @Column(name = "date_created")
+    @CreationTimestamp
     private Date dateCreated ;
+
+    @Column(name = "last_updated")
+    @UpdateTimestamp
     private Date lastUpdated ;
 }
