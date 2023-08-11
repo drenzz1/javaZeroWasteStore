ea21968bdff121866467de287a780c2aaab215bc
drenzz1 <you@example.com>
2023-08-11 10:23:21 +0200

    Dreni Mac Commit

--- angular-zerowaste/src/app/components/product-list/product-list.component.ts
+++ angular-zerowaste/src/app/components/product-list/product-list.component.ts
@@ -4,7 +4,7 @@ import {ProductService} from "../../service/product.service";
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
+  templateUrl: './product-list-table.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit{
