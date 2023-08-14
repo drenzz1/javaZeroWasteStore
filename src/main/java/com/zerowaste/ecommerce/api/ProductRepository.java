69777ade071ff301e5a7be7600f13a5dac430e31
drenzz1 <you@example.com>
2023-08-14 13:53:12 +0200

    Dreni Mac Commit

--- src/main/java/com/zerowaste/ecommerce/api/ProductRepository.java
+++ src/main/java/com/zerowaste/ecommerce/api/ProductRepository.java
@@ -9,5 +9,5 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product,Long> {
     Page <Product> findByCategoryId(@Param("id") Long id , Pageable pageable); //Query method Select * from product where category_id ?
-
+    Page <Product> findByNameContaining(@Param("name") String name , Pageable pageable);
 }
