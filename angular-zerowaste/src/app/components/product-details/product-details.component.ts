5d66588bcea455442163c23d615ea4f6667c6e01
drenzz1 <you@example.com>
2023-08-26 20:07:54 +0200

    Dreni Mac Commit

--- angular-zerowaste/src/app/components/product-details/product-details.component.ts
+++ angular-zerowaste/src/app/components/product-details/product-details.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {Product} from "../../common/product";
 import {ProductService} from "../../service/product.service";
 import {ActivatedRoute} from "@angular/router";
+import {CartService} from "../../service/cart.service";
+import {CartItem} from "../../common/cart-item";
 
 @Component({
   selector: 'app-product-details',
@@ -11,6 +13,7 @@ import {ActivatedRoute} from "@angular/router";
 export class ProductDetailsComponent implements OnInit{
   product!:Product;
   constructor(private productService:ProductService,
+              private cartService:CartService,
               private route : ActivatedRoute) {
   }
   ngOnInit() {
@@ -26,5 +29,10 @@ export class ProductDetailsComponent implements OnInit{
         this.product = data;
       }
     )
+  }
+  addToCart(){
+    console.log(`Addint to cart ${this.product.name},${this.product.unitPrice}`);
+    const theCartItem = new CartItem(this.product);
+    this.cartService.addToCart(theCartItem);
   }
 }
