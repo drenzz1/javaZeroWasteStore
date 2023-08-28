6706e6bdb967a4ab3fe9ea647ca34e09361d5ead
drenzz1 <you@example.com>
2023-08-28 23:03:13 +0200

    Dreni Mac Commit

--- angular-zerowaste/src/app/components/checkout/checkout.component.ts
+++ angular-zerowaste/src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {Form, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import { FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {ZerowasteFormService} from "../../service/zerowaste-form.service";
 import {Country} from "../../common/country";
 import {State} from "../../common/state";
